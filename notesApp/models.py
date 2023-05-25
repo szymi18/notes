da6022141c4fa34120ff3199e2187e4b0f68cd27
Szymon Michna <szymek0705@gmail.com>
2023-05-25 19:54:15 +0200

    add Note to models.py and migrate

--- notesApp/models.py
+++ notesApp/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
