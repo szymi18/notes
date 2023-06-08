4f0153b240a20aa9111f86bdca5e039e06cd0c5c
Szymon Michna <szymek0705@gmail.com>
2023-06-08 20:07:46 +0200

    change file urls.py in notesApp and urls.py in notes

--- notesApp/urls.py
+++ notesApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.note_list, name="note_list"),
+    path('note/<int:pk>/',views.note_detail, name="note_detail"),
+    path('note/<int:pk>/edit/', views.note_edit, name='note_edit'),
+    path('note/new/', views.note_create, name='note_create'),
 ]
