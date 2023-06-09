37c19d2ce25ea9f9f52e3c327a79b2e3f06695ea
Szymon Michna <szymek0705@gmail.com>
2023-06-10 00:56:16 +0200

    add Note to admin.py

--- notesApp/admin.py
+++ notesApp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 
+from notesApp.models import Note
 # Register your models here.
 
+@admin.register(Note)
+class adminNote(admin.ModelAdmin):
+    list_display = ('title', 'content', 'creation_date', 'author')
